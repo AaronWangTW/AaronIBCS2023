618b7977c9d186c1c1d3bf95526c4f902114a1b7
AaronWangTW <68408616+AaronWangTW@users.noreply.github.com>
2021-10-26 09:54:21 +0800

    function updated and funcExercises complete

---
+++ Intro/funcs/funcsExcercise.py
@@ -0,0 +1,119 @@
+# 1. Calculate Factorial
+def factorial(num: int) -> int:
+    result = num
+    if num > 0:
+        for i in range(1, num):
+            result *= i
+        return result
+    elif num == 0:
+        return 1
+    return False
+
+
+print(factorial(5))
+
+# 2. Calculate Permutation
+# n! / (n-r)!
+# nPr(10,10) -> 3628800
+
+
+def nPr(n: int, r: int) -> int:
+    return int(factorial(n) / factorial(n-r))
+
+
+print(nPr(3, 2))
+
+# 3. Calculate Combination
+# n! / (r!(n-r)!)
+# nCr(10,10) -> 1
+
+
+def nCr(n: int, r: int) -> int:
+    return int(factorial(n)/(factorial(r)*factorial(n-r)))
+
+
+print(nCr(10, 10))
+
+# pascalsTriangle(rows)
+# pascalsTriangle(3) -> [[1],[1,1][1,2,1]]
+
+
+def pascalsTriangle(row: int) -> list:
+    layer = [1]
+    result = []
+    for _ in range(row):
+        temp = []
+        for j in range(len(layer)):
+            if j == 0:
+                temp += [1]
+            else:
+                temp += [layer[j]+layer[j-1]]
+        temp += [1]
+        layer = temp.copy()
+        result.append(temp)
+    return result
+
+
+print(pascalsTriangle(3))
+
+
+# Generator func with star
+# starGen()
+# *
+# **
+# ***
+def starGen():
+    i = 1
+    while True:
+        yield i*"*"
+        i += 1
+
+
+stars = starGen()
+collect = []
+for i in range(5):
+    collect += [next(stars)]
+print(collect)
+
+
+# PrimeNumberGenerator
+# Seive of eratosthenes
+def primeNumGen():
+    past = []
+    i = 2
+    while True:
+        isPrime = True
+        for j in past:
+            if i % j == 0:
+                isPrime = False
+        if isPrime:
+            past += [i]
+            yield i
+        i += 1
+
+
+prime = primeNumGen()
+collect = []
+for i in range(5):
+    collect += [next(prime)]
+print(collect)
+
+
+# Pascal Triangle, but Generator, returns next row each time
+def pascalTriangleGen(row:int)->list:
+    layer = [1]
+    for _ in range(row):
+        temp = []
+        for j in range(len(layer)):
+            if j == 0:
+                temp += [1]
+            else:
+                temp += [layer[j]+layer[j-1]]
+        temp += [1]
+        layer = temp.copy()
+        yield temp
+triangle=pascalTriangleGen(3)
+collect=[]
+for i in range(3):
+    collect.append(next(triangle))
+print(collect)
--- Intro/funcs/function.py
+++ Intro/funcs/function.py
@@ -52,15 +52,80 @@ def nthRoot(num: float, root: int, precision: float = 1e-20) -> float:
     if root == 1:
         return num
 
-    def f(x:float, r:int,g:float):
+    def f(x: float, r: int, g: float):
         return (g ** r - x)/(r * g ** (r-1))
     # initial guess at an answer, there's more advanced way to choose this
     guess = 1
     prev = 0
-    
+
     while abs(guess - prev) > precision:
         prev = guess
-        guess -= f(num,root,guess)
+        guess -= f(num, root, guess)
     return guess
 
-print(nthRoot(81,2))
+
+print(nthRoot(81, 2))
+print(nthRoot(root=2, num=4))
+
+# In python, functions are first-class members
+myvar = nthRoot
+print(myvar(64, 2))
+
+#Call back functions, functions that accept function as input
+
+
+def evenHandler(val: int) -> bool:
+    print(f"{val} is divisible by 2")
+
+
+def oddHandler(val: int) -> bool:
+    val = val+1
+    print(f"Value is now {val}. Much better.")
+
+
+def doThing(evenCallback, oddCallback) -> None:
+    for i in range(20):
+        if i % 2 == 0:
+            evenCallback(i)
+        else:
+            oddCallback(i)
+
+#doThing(evenHandler,oddHandler)
+
+# Generator
+# yield, basically return but iterable and temporarily stopped. Without iterating on it, it won't return value
+# use next() to get value from it since it returns a generator object
+
+
+def squares():
+    n = 1
+    while True:
+        yield n ** 2
+        n += 1
+
+
+val = squares()
+collect = []
+print(type(val))
+for i in range(5):
+    collect += [next(val)]
+
+for i in range(7):
+    collect += [next(val)]
+print(collect)
+# the iteration continues
+
+
+def fibonacciNumberse(nums):
+    x, y = 0, 1
+    for _ in range(nums):
+        x, y = y, x+y
+        yield x
+
+
+numberse1 = fibonacciNumberse(10)
+collect = []
+for i in range(10):
+    collect += [next(numberse1)]
+print(collect)
+# generator is memory efficient because it doesn't store all of the variables and used variable

61f816741ded8493d5a3c35ca0758740a14b1dd8
AaronWangTW <68408616+AaronWangTW@users.noreply.github.com>
2022-05-11 14:17:53 +0800

    chat software basis

--- networking/ConsoleChat/main.py
+++ networking/ConsoleChat/main.py
@@ -2,17 +2,20 @@ import argparse
 from os import chdir, path
 import sys
 
-from net.serverProtocol import runServer
-
+from net import runServer
+from chatClient import runClient
 
 def main(args: argparse.Namespace):
     host = args.host if args.host else "127.0.0.1"
     port = args.port if args.port else 5001
+    test = args.test
 
     if args.server:
         print("Starting server...")
         runServer(host, port)
-
+        return
+    
+    runClient(host=host,port = port, test = test)
 
 if __name__ == "__main__":
     chdir(path.dirname(path.abspath(__file__)))
@@ -28,5 +31,6 @@ if __name__ == "__main__":
                         help="Specify hostname for client/server")
     parser.add_argument("--port", type=int,
                         help="Specify port for client/server")
+    parser.add_argument("--test","-t", default=False, action="store_true",help="Start chat client in testing mode")
     arguments = parser.parse_args(sys.argv[1:])
     main(args=arguments)
